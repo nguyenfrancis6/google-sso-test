ac25d13e8eaccc148a8b5aa93d61b4adff2100ac
Francis Nguyen <nguyenfrancis6@gmail.com>
2025-07-09 09:04:38 -0700

    fixed type errors

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,6 +5,10 @@ import GoogleProvider from 'next-auth/providers/google'
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
+if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
+    throw new Error('Missing Google OAuth credentials');
+}
+
 const authOption: NextAuthOptions = {
     session: {
         strategy: 'jwt' 

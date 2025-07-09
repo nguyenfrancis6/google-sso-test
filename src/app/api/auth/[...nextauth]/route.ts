5d39be75099d26d27443e9af4ee6aa61b0122968
Francis Nguyen <nguyenfrancis6@gmail.com>
2025-07-09 08:56:24 -0700

    Implement Google SSO on test project

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,31 @@
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth";
+import GoogleProvider from 'next-auth/providers/google'
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+const authOption: NextAuthOptions = {
+    session: {
+        strategy: 'jwt' 
+    },
+    providers: [
+        GoogleProvider({
+            clientId: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET
+        })
+    ],
+    // // callback we care most about is sign in callback, 
+    // callbacks: {
+    //     async signIn({ account, profile }) {
+    //         if (!profile?.email){
+    //             throw new Error('No profile')
+    //         }
+
+    //         await // people use DB to check user and email, we would need to use s3 here.
+    //     }
+    // }
+}
+
+const handler = NextAuth(authOption)
+export { handler as GET, handler as POST }
